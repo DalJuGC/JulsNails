11062baa4e86d34525186d08bc7904455c719ebc
Dalia Julissa Galindo Castillo <135063081+DalJuGC@users.noreply.github.com>
2023-06-08 14:50:01 -0600

    Add files via upload

---
+++ src/main/java/controlador/Empleado/SVBsEmpleado.java
@@ -0,0 +1,4 @@
+package controlador.Empleado;
+
+public class SVBsEmpleado {
+}
---
+++ src/main/java/controlador/Empleado/SVMEmpleado.java
@@ -0,0 +1,53 @@
+package controlador.Empleado;
+
+import modelo.Empleado;
+import datos.EmpleadoDAO;
+
+import javax.annotation.Resource;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@WebServlet(name = "SVMEmpleado", urlPatterns = {"/SVMEmpleado"})
+public class SVMEmpleado extends HttpServlet {
+    @Resource(name = "jdbc/database")
+    private DataSource conexion;
+
+    private int Codigo;
+    private String Nombre;
+    private String Cargo;
+    private String Telefono;
+    private String Domicilio;
+    private String Fech_Con;
+
+    @Override
+    protected void doPost(HttpServletRequest rq, HttpServletResponse rs) throws IOException{
+        Codigo = (int)rq.getSession().getAttribute("Codigo");
+        Nombre = rq.getParameter("Nombre");
+        Cargo = rq.getParameter("Cargo");
+        Telefono = rq.getParameter("Telefono");
+        Domicilio = rq.getParameter("Domicilio");
+        Fech_Con = rq.getParameter("Fech_Con");
+        try{
+            Connection connection = conexion.getConnection();
+            EmpleadoDAO empDAO = new EmpleadoDAO();
+            Empleado emp = new Empleado();
+            rq.getSession().setAttribute("datos", emp);
+            if(Nombre != null && Cargo != null && Telefono != null && Domicilio != null && Fech_Con != null){
+                empDAO.modificarNom(Codigo, Nombre);
+                empDAO.modificarCar(Codigo, Cargo);
+                empDAO.modificarTel(Codigo, Telefono);
+                empDAO.modificarDom(Codigo, Domicilio);
+                empDAO.modificarFech(Codigo, Fech_Con);
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        rs.sendRedirect("/ProyectoDAW/Empleado/ModificarEmp.jsp");
+    }
+}
