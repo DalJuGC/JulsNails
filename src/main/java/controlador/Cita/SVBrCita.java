24cc3785043e86172909cea6d1c6e1ff40817a52
Dalia Julissa Galindo Castillo <135063081+DalJuGC@users.noreply.github.com>
2023-06-08 21:42:37 -0600

    Add files via upload

--- src/main/java/controlador/Cita/SVBrCita.java
+++ src/main/java/controlador/Cita/SVBrCita.java
@@ -1,5 +1,36 @@
 package controlador.Cita;
 
-public class SVBrCita {
-    
+import datos.CitaDAO;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.io.IOException;
+
+@WebServlet(name = "SVBrCita", urlPatterns = {"/SVBrCita"})
+public class SVBrCita extends HttpServlet{
+    @Resource(name = "jdbc/database")
+    private DataSource conexion;
+
+    private int Codigo;
+
+    @Override
+    protected void doPost(HttpServletRequest rq, HttpServletResponse rs) throws IOException{
+        Codigo = Integer.parseInt(rq.getParameter("Codigo"));
+        try{
+            Connection connection = conexion.getConnection();
+            CitaDAO citaDAO = new CitaDAO();
+            citaDAO.borrar(Codigo);
+            connection.close();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        rs.sendRedirect("/ProyectoDAW/Cita/ListaCita.jsp");
+    }
 }
