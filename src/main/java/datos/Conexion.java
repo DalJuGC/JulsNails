71845ba97db46e0496c728812294db826b09797e
Dalia Julissa Galindo Castillo <135063081+DalJuGC@users.noreply.github.com>
2023-06-11 10:13:04 -0600

    Add files via upload

--- src/main/java/datos/Conexion.java
+++ src/main/java/datos/Conexion.java
@@ -1,31 +1,28 @@
 package datos;
 
 import java.sql.*;
-
 public class Conexion {
     private static String user = "postgres";
     private static String pswd = "password";
-    private static String bd = "ProyectoDAW";
+    private static String bd = "JulsNails";
     private static String server = "jdbc:postgresql://localhost:5432/" + bd;
     private static String driver = "org.postgresql.Driver";
 
     public static Connection getConnection() {
-
-        try {
+        try{
             Class.forName(driver);
-            //System.out.println("Conexion establecida");
             return DriverManager.getConnection(server, user, pswd);
         } catch (SQLException e) {
             System.out.println("Error al intentar conectarse a la BD" + server);
         } catch (ClassNotFoundException e) {
             System.out.println("Error");
+
         }
         return null;
     }
 
-
     public static void close(ResultSet result){
-        try {
+        try{
             result.close();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -33,7 +30,7 @@ public class Conexion {
     }
 
     public static void close(Statement state){
-        try {
+        try{
             state.close();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -41,9 +38,9 @@ public class Conexion {
     }
 
     public static void close(Connection conn){
-        try {
+        try{
             conn.close();
-        } catch (SQLException e) {
+        } catch (SQLException e){
             e.printStackTrace();
         }
     }
