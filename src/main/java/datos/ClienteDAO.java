a54446d3b9c24f207b9bdff01a82205f74126fa0
Dalia Julissa Galindo Castillo <135063081+DalJuGC@users.noreply.github.com>
2023-06-11 10:14:26 -0600

    Add files via upload

--- src/main/java/datos/ClienteDAO.java
+++ src/main/java/datos/ClienteDAO.java
@@ -3,117 +3,125 @@ package datos;
 import modelo.Cliente;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.sql.*;
 
-public class ClienteDAO{
-    public static final String selectSQL = "SELECT * FROM Cliente";
-    public static final String insertSQL = "INSERT INTO Cliente(Codigo, Nombre, Telefono) VALUES (?,?,?);";
-    public static final  String updateSQL = "UPDATE Cliente SET Codigo = ?, Nombre = ?, Telefono = ? WHERE Codigo = ? ";
-    public static final String deleteSQL = "DELETE FROM Cliente WHERE Codigo = ? ";
-    public static final String consultSQL = "SELECT * FROM Cliente WHERE Codigo = ?";
+
+public class ClienteDAO {
+    public static final String selectSQL = "SELECT * FROM cliente";
+    public static final String insertSQL = "INSERT INTO cliente(codigo, nombre, telefono, domicilio) VALUES (?,?,?,?)";
+    public static final String updateSQL = "UPDATE cliente SET nombre = ?, telefono = ?, domicilio = ? WHERE codigo = ?";
+    public static final String deleteSQL = "DELETE FROM cliente WHERE codigo = ? ";
 
     private Connection connection;
     private PreparedStatement state;
     private ResultSet result;
-    private Cliente cli;
+    private Cliente cliente;
 
     public ClienteDAO(Connection connection) {
         this.connection = connection;
     }
 
-    //Muestra la lista de clientes registrados
-    public ArrayList<Cliente> listar() throws SQLException{
-        ArrayList<Cliente> lista = new ArrayList<>();
-        connection = Conexion.getConnection();
-        state = (PreparedStatement) connection.createStatement();
-        result = state.executeQuery(selectSQL);
+    //Lista los clientes registrados
+    public List<Cliente> listar() {
+        List<Cliente> cli = new ArrayList<>();
 
-         while (result.next()){
-            int Codigo = result.getInt("codigo");
-            String Nombre = result.getString("combre");
-            String Telefono = result.getString("telefono");
+        try {
+            connection = Conexion.getConnection();
+            state = connection.prepareStatement(selectSQL);
+            result = state.executeQuery();
 
-            cli = new Cliente(Codigo, Nombre, Telefono);
-            lista.add(cli);
+            while (result.next()) {
+                int codigo = result.getInt("codigo");
+                String nombre = result.getString("nombre");
+                String telefono = result.getString("telefono");
+                String domicilio = result.getString("domicilio");
+
+                Cliente cliente = new Cliente(codigo, nombre, telefono, domicilio);
+                cli.add(cliente);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        Conexion.close(connection);
-        Conexion.close(state);
-        Conexion.close(result);
-        return lista;
+        return cli;
     }
 
-    //Agrega un cliente más a la tabla
-    public void insertar(Cliente cliente) throws SQLException{
+    //Agrega un cliente nuevo a la tabla
+    public void insertar(Cliente cliente) {
+        try {
+            connection = Conexion.getConnection();
+            state = connection.prepareStatement(insertSQL);
 
-        connection = Conexion.getConnection();
-        state = connection.prepareStatement(insertSQL);
+            state.setString(1, cliente.getNombre());
+            state.setString(2, cliente.getTelefono());
+            state.setString(3, cliente.getDomicilio());
+            state.executeUpdate();
 
-        state.setInt(1, cliente.getCodigo());
-        state.setString(2, cliente.getNombre());
-        state.setString(3, cliente.getTelefono());
-        state.executeUpdate();
-
-        Conexion.close(connection);
-        Conexion.close(state);
-    }
-
-    //Modifica el nombre del cliente
-    public void modificarNom(int Codigo, String Nombre) throws SQLException{
-
-        connection = Conexion.getConnection();
-        state = connection.prepareStatement(updateSQL);
-
-        state.setString(1, Nombre);
-        state.setInt(3, Codigo);
-        state.executeUpdate();
-
-        Conexion.close(connection);
-        Conexion.close(state);
-    }
-
-    //Modifica el numero de telefono del cliente
-    public void modificarTel(int Codigo, String Telefono) throws SQLException{
-
-        connection = Conexion.getConnection();
-        state = connection.prepareStatement(updateSQL);
-
-        state.setString(1, Telefono);
-        state.setInt(3, Codigo);
-        state.executeUpdate();
-
-        Conexion.close(connection);
-        Conexion.close(state);
-    }
-
-    //Busca un cliente por su Codigo
-    public Cliente buscar(int Codigo) throws SQLException{
-
-        connection = Conexion.getConnection();
-        state = connection.prepareStatement(consultSQL);
-        state.setInt(1, Codigo);
-
-        if (result.next()){
-            Conexion.close(result);
-            Conexion.close(state);
+            connection.close();
+            state.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+    }
 
-        Cliente cliente = new Cliente(result.getInt("Codigo"),
-                result.getString("Nombre"),
-                result.getString("Telefono"));
-        Conexion.close(result);
-        Conexion.close(state);
+    //Modifica un cliente de la tabla
+    public void modificar(Cliente cliente) {
+        try {
+            connection = Conexion.getConnection();
+            state = connection.prepareStatement(updateSQL);
+
+            state.setString(1, cliente.getNombre());
+            state.setString(2, cliente.getTelefono());
+            state.setString(3, cliente.getDomicilio());
+            state.setInt(4, cliente.getCodigo());
+            state.executeUpdate();
+
+            connection.close();
+            state.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Busca un cliente por su codigo
+    public Cliente buscar(int cod) {
+        String consultSQL = "SELECT * FROM cliente WHERE codigo = ?" + cod;
+
+        try {
+            connection = Conexion.getConnection();
+            state = connection.prepareStatement(consultSQL);
+            //state.setInt(1, codigo);
+            result = state.executeQuery();
+
+            if (result.next()) {
+                int codigo = result.getInt("codigo");
+                String nombre = result.getString("nombre");
+                String telefono = result.getString("telefono");
+                String domicilio = result.getString("domicilio");
+
+                cliente = new Cliente(codigo, nombre, telefono, domicilio);
+            }
+
+            connection.close();
+            state.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return cliente;
     }
 
-    //Borra un cliente
-    public void borrar(int Codigo) throws SQLException{
+    //Borra un cliente de la tabla
+    public void borrar(int codigo) {
+        try {
+            connection = Conexion.getConnection();
+            state = connection.prepareStatement(deleteSQL);
+            state.setInt(1, codigo);
+            state.executeUpdate();
 
-        connection = Conexion.getConnection();
-        state = connection.prepareStatement(deleteSQL);
-
-        state.setInt(1,Codigo);
-        state.executeUpdate();
-        
-        Conexion.close(state);
+            connection.close();
+            state.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
