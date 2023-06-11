10b65e67a3e4cdbf8e6707075c0b888b449c1ee1
Dalia Julissa Galindo Castillo <135063081+DalJuGC@users.noreply.github.com>
2023-06-11 10:09:05 -0600

    Add files via upload

--- src/main/java/datos/TratamientoDAO.java
+++ src/main/java/datos/TratamientoDAO.java
@@ -1,181 +1,132 @@
 package datos;
 
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-
 import modelo.Empleado;
 import modelo.Tratamiento;
 
-public class TratamientoDAO{
-    public static final String selectSQL = "SELECT * FROM Tratamiento";
-    public static final String insertSQL = "INSERT INTO Tratamiento(Nombre, Precio, Cod_Manicurista) VALUES (?,?,?)";
-    public static final  String updateSQL = "UPDATE Tratamiento SET Nombre = ?, Precio = ?, Cod_Manicurista = ? WHERE Codigo = ? ";
-    public static final String deleteSQL = "DELETE FROM Tratamiento WHERE Codigo = ? ";
-    public static final String consultSQL = "SELECT * FROM Tratamiento WHERE Codigo = ?";
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.*;
 
-    public List<Tratamiento> seleccionar(){
-        Connection conn = null;
-        Statement state = null;
-        ResultSet result = null;
-        Tratamiento Trat = null;
 
-        List<Tratamiento> Tratamientos = new ArrayList<>();
-        try{
-            conn = Conexion.getConnection();
-            state = conn.createStatement();
-            result = state.executeQuery(selectSQL);
+public class TratamientoDAO {
+    public static final String selectSQL = "SELECT Tratamiento.codigo, Tratamiento.nombre AS Tratamiento, Tratamiento.precio, Empleado.nombre AS Empleado FROM Tratamiento\n" +
+            "JOIN Empleado ON Tratamiento.cod_empleado = Empleado.codigo";
+    public static final String insertSQL = "INSERT INTO tratamiento(codigo, nombre, precio, cod_empleado) VALUES (?,?,?,?)";
+    public static final String updateSQL = "UPDATE tratamiento SET nombre = ?, precio = ?, cod_empleado = ? WHERE codigo = ?";
+    public static final String deleteSQL = "DELETE FROM tratamiento WHERE codigo = ? ";
 
-            while(result.next()){
-                int Codigo = result.getInt("Codigo");
-                String Nombre = result.getString("Nombre");
-                String Precio = result.getString("Precio");
-                String Cod_Empleado =result.getString("Cod_Empleado");
-                
-                Trat = new Tratamiento(Codigo, Nombre, Precio, Cod_Empleado);
-                Tratamientos.add(Trat);
+    private Connection connection;
+    private PreparedStatement state;
+    private ResultSet result;
+    private Tratamiento tratamiento;
+    private Empleado empleado;
+
+    public TratamientoDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    //Lista los tratamiento registrados
+    public List<Tratamiento> listar() {
+        List<Tratamiento> trat =  new ArrayList<>();
+
+        try {
+            connection = Conexion.getConnection();
+            state = connection.prepareStatement(selectSQL);
+            result = state.executeQuery();
+
+            while (result.next()) {
+                String nombre = result.getString("Empleado");
+                empleado = new Empleado(nombre);
+
+                int codigo = result.getInt("codigo");
+                nombre = result.getString("nombre");
+                Float precio = result.getFloat("telefono");
+                int cod_empleado = result.getInt("cod_empleado");
+
+                Tratamiento tratamiento = new Tratamiento(codigo, nombre, precio, cod_empleado);
+                trat.add(tratamiento);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return trat;
+    }
+
+    //Agrega un tratamiento nuevo a la tabla
+    public void insertar(Tratamiento tratamiento) {
+        try {
+            connection = Conexion.getConnection();
+            state = connection.prepareStatement(insertSQL);
+
+            state.setString(1, tratamiento.getNombre());
+            state.setFloat(2, tratamiento.getPrecio());
+            state.setInt(3, tratamiento.getCod_empleado());
+            state.executeUpdate();
+
+            connection.close();
+            state.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Modifica un tratamiento de la tabla
+    public void modificar(Tratamiento tratamiento) {
+        try {
+            connection = Conexion.getConnection();
+            state = connection.prepareStatement(updateSQL);
+
+            state.setString(1, tratamiento.getNombre());
+            state.setFloat(2, tratamiento.getPrecio());
+            state.setInt(3, tratamiento.getCod_empleado());
+            state.setInt(4, tratamiento.getCodigo());
+            state.executeUpdate();
+
+            connection.close();
+            state.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Busca un tratamiento por su codigo
+    public Tratamiento buscar(int cod) {
+        String consultSQL = "SELECT * FROM tratamiento WHERE codigo = ?" + cod;
+
+        try {
+            connection = Conexion.getConnection();
+            state = connection.prepareStatement(consultSQL);
+            //state.setInt(1, codigo);
+            result = state.executeQuery();
+
+            if (result.next()) {
+                int codigo = result.getInt("codigo");
+                String nombre = result.getString("nombre");
+                Float precio = result.getFloat("precio");
+                int cod_empleado = result.getInt("cod_empleado");
+
+                tratamiento = new Tratamiento(codigo, nombre, precio, cod_empleado);
             }
 
-            Conexion.close(result);
-            Conexion.close((ResultSet) state);
-            Conexion.close(conn);
-
-            for(Tratamiento tratamientos: Tratamientos){
-                System.out.println("Código: " + tratamientos.getCodigo());
-                System.out.println("Nombre: " + tratamientos.getNombre());
-                System.out.println("Precio: " + tratamientos.getPrecio());
-                System.out.println(" \n ");
-            }
-
-        }catch (Exception e) {
-            e.printStackTrace(System.out);
-        } 
-        return Tratamientos;
-    }
-
-    public void insertar(Tratamiento tratamiento){
-
-        Connection conn = null;
-        PreparedStatement state = null;
-        int registros=0;
-
-        try{
-            conn = Conexion.getConnection();
-            state = conn.prepareStatement(insertSQL);
-
-            state.setString(1,tratamiento.getNombre());
-            state.setString(2,tratamiento.getPrecio());
-
-            registros = state.executeUpdate();
-
-            if(registros>0)
-                System.out.println("Registtro agregado correctamente");
-
-            Conexion.close(state);
-            Conexion.close(conn);
-
-        }catch (Exception e) {
+            connection.close();
+            state.close();
+        } catch (SQLException e) {
             e.printStackTrace();
         }
-    }
-
-    public void modificarNombre(int Codigo, String Nombre){
-
-        Connection conn = null;
-        PreparedStatement state = null;
-        int registros=0;
-
-
-        try{
-            conn = Conexion.getConnection();
-            state = conn.prepareStatement(updateSQL);
-
-            state.setString(1,Nombre);
-            state.setFloat(2,Codigo);
-
-            registros = state.executeUpdate();
-
-            if(registros>0)
-                System.out.println("Registro actualizado");
-
-
-            Conexion.close(state);
-            Conexion.close(conn);
-
-        }catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void modificarPrecio(int Codigo, String Precio){
-
-        Connection conn = null;
-        PreparedStatement state = null;
-        int registros=0;
-
-
-        try{
-            conn = Conexion.getConnection();
-            state = conn.prepareStatement(updateSQL);
-
-            state.setString(1,Precio);
-            state.setInt(2,Codigo);
-
-            registros = state.executeUpdate();
-
-            if(registros>0)
-                System.out.println("Registro actualizado");
-
-
-            Conexion.close(state);
-            Conexion.close(conn);
-
-        }catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public Tratamiento buscar(int Codigo) throws SQLException{
-
-        Connection conn = null;
-        PreparedStatement state = null;
-        ResultSet result = null;
-
-        conn = Conexion.getConnection();
-        state = conn.prepareStatement(consultSQL);
-
-        Tratamiento tratamiento = new Tratamiento(result.getInt("Codigo"),
-                result.getString("Nombre"),
-                result.getString("Precio"),
-                result.getString("Cod_Empleado"));
-        Conexion.close(result);
-        Conexion.close(state);
-
         return tratamiento;
     }
 
-    public void borrar(int Codigo){
+    //Borra un tratamiento de la tabla
+    public void borrar(int codigo) {
+        try {
+            connection = Conexion.getConnection();
+            state = connection.prepareStatement(deleteSQL);
+            state.setInt(1, codigo);
+            state.executeUpdate();
 
-        Connection conn = null;
-        PreparedStatement state = null;
-        int registros=0;
-
-        try{
-            conn = Conexion.getConnection();
-            state = conn.prepareStatement(deleteSQL);
-
-            state.setInt(1,Codigo);
-
-            registros = state.executeUpdate();
-
-            if(registros>0)
-                System.out.println("Registro eliminado");
-
-
-            Conexion.close(state);
-            Conexion.close(conn);
-
-        }catch (Exception e) {
+            connection.close();
+            state.close();
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
