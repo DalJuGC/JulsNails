b48c7f0f604d9b703a6584165aac7bf5f1798677
Dalia Julissa Galindo Castillo <135063081+DalJuGC@users.noreply.github.com>
2023-06-11 10:11:31 -0600

    Add files via upload

--- src/main/java/datos/EmpleadoDAO.java
+++ src/main/java/datos/EmpleadoDAO.java
@@ -1,220 +1,134 @@
 package datos;
 
-import modelo.Cliente;
 import modelo.Empleado;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.sql.*;
 
 public class EmpleadoDAO {
-    public static final String selectSQL = "SELECT * FROM Empleado";
-    public static final String insertSQL = "INSERT INTO Empleado(Codigo, Nombre, Cargo, Telefono, Domicilio, Fech_Con) VALUES (?,?,?,?,?,?);";
-    public static final String updateSQL = "UPDATE Empleado SET Codigo = ?, Nombre = ?, Cargo = ?, Telefono = ?, Domicilio = ?, Fech_Con = ?";
-    public static final String deleteSQL = "DELETE FROM Empleado WHERE Codigo = ?";
-    public static final String consultSQL = "SELECT * FROM Empleado WHERE Codigo = ?";
+    public static final String selectSQL = "SELECT * FROM empleado";
+    public static final String insertSQL = "INSERT INTO empleado(codigo, nombre, cargo, telefono, domicilio, fecha_contrato) VALUES (?,?,?,?,?,?)";
+    public static final String updateSQL = "UPDATE empleado SET nombre = ?, cargo = ?, telefono = ?, domicilio = ? fecha_contrato = ? WHERE codigo = ?";
+    public static final String deleteSQL = "DELETE FROM empleado WHERE codigo = ? ";
 
     private Connection connection;
+    private PreparedStatement state;
+    private ResultSet result;
+    private Empleado empleado;
 
-    public EmpleadoDAO(Connection connection){this.connection = connection;}
-    //Muestra los clientes
-    public ArrayList<Empleado> seleccionar() throws SQLException{
-        ArrayList<Empleado> lista = new ArrayList<>();
-        Connection conn = null;
-        Statement state = null;
-        ResultSet result = null;
-        Empleado emp = null;
-        try{
-            conn = Conexion.getConnection();
-            state = conn.createStatement();
-            result = state.executeQuery(selectSQL);
+    public EmpleadoDAO(Connection connection) {
+        this.connection = connection;
+    }
 
-            while(result.next()){
-                int Codigo = result.getInt("Codigo");
-                String Nombre = result.getString("Nombre");
-                String Cargo = result.getString("Cargo");
-                String Telefono = result.getString("Telefono");
-                String Domicilio = result.getString("Domicilio");
-                String Fech_Con = result.getString("Fech_Con");
+    //Lista los empleado registrados
+    public List<Empleado> listar() {
+        List<Empleado> emp = new ArrayList<>();
 
-                emp = new Empleado(Codigo, Nombre, Cargo, Telefono, Domicilio, Fech_Con);
-                lista.add(emp);
+        try {
+            connection = Conexion.getConnection();
+            state = connection.prepareStatement(selectSQL);
+            result = state.executeQuery();
+
+            while (result.next()) {
+                int codigo = result.getInt("codigo");
+                String nombre = result.getString("nombre");
+                String cargo = result.getString("cargo");
+                String telefono = result.getString("telefono");
+                String domicilio = result.getString("domicilio");
+                Date fecha_contrato = result.getDate("fecha_contrato");
+
+                Empleado empleado = new Empleado(codigo, nombre, cargo, telefono, domicilio, fecha_contrato);
+                emp.add(empleado);
             }
-            Conexion.close(result);
-            Conexion.close(state);
-            Conexion.close(conn);
-        }catch (Exception e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
-        return lista;
+        return emp;
     }
 
-    public void insertar(Empleado empleado) throws SQLException{
-        Connection conn = null;
-        PreparedStatement state = null;
+    //Agrega un empleado nuevo a la tabla
+    public void insertar(Empleado empleado) {
+        try {
+            connection = Conexion.getConnection();
+            state = connection.prepareStatement(insertSQL);
 
-        conn = Conexion.getConnection();
-        state = conn.prepareStatement(insertSQL);
+            state.setString(1, empleado.getNombre());
+            state.setString(2, empleado.getCargo());
+            state.setString(3, empleado.getTelefono());
+            state.setString(4, empleado.getDomicilio());
+            state.setDate(5, empleado.getFecha_contrato());
+            state.executeUpdate();
 
-        state.setInt(1,empleado.getCodigo());
-        state.setString(2, empleado.getNombre());
-        state.setString(3, empleado.getCargo());
-        state.setString(4, empleado.getTelefono());
-        state.setString(5, empleado.getDomicilio());
-        state.setString(6, empleado.getFech_Con());
-    }
-
-    public void modificarNom(int Codigo, String Nombre) throws SQLException{
-        Connection conn = null;
-        PreparedStatement state = null;
-        int registros = 0;
-
-        try{
-            conn = Conexion.getConnection();
-            state = conn.prepareStatement(updateSQL);
-
-            state.setString(1, Nombre);
-            state.setInt(2, Codigo);
-
-            registros = state.executeUpdate();
-            if(registros>0)
-                System.out.println("Registro actualizado");
-
-            Conexion.close(state);
-            Conexion.close(conn);
-        }catch (SQLException e) {
+            connection.close();
+            state.close();
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public void modificarCar(int Codigo, String Cargo) throws SQLException{
-        Connection conn = null;
-        PreparedStatement state = null;
-        int registros = 0;
+    //Modifica un empleado de la tabla
+    public void modificar(Empleado empleado) {
+        try {
+            connection = Conexion.getConnection();
+            state = connection.prepareStatement(updateSQL);
 
-        try{
-            conn = Conexion.getConnection();
-            state = conn.prepareStatement(updateSQL);
+            state.setString(1, empleado.getNombre());
+            state.setString(2, empleado.getCargo());
+            state.setString(3, empleado.getTelefono());
+            state.setString(4, empleado.getDomicilio());
+            state.setDate(5, empleado.getFecha_contrato());
+            state.setInt(6, empleado.getCodigo());
+            state.executeUpdate();
 
-            state.setString(1, Cargo);
-            state.setInt(2, Codigo);
-
-            registros = state.executeUpdate();
-            if(registros>0)
-                System.out.println("Registro actualizado");
-
-            Conexion.close(state);
-            Conexion.close(conn);
-        }catch (SQLException e) {
+            connection.close();
+            state.close();
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public void modificarTel(int Codigo, String Telefono) throws SQLException{
-        Connection conn = null;
-        PreparedStatement state = null;
-        int registros = 0;
+    //Busca un empleado por su codigo
+    public Empleado buscar(int cod) {
+        String consultSQL = "SELECT * FROM empleado WHERE codigo = ?" + cod;
 
-        try{
-            conn = Conexion.getConnection();
-            state = conn.prepareStatement(updateSQL);
+        try {
+            connection = Conexion.getConnection();
+            state = connection.prepareStatement(consultSQL);
+            //state.setInt(1, codigo);
+            result = state.executeQuery();
 
-            state.setString(1, Telefono);
-            state.setInt(2, Codigo);
+            if (result.next()) {
+                int codigo = result.getInt("codigo");
+                String nombre = result.getString("nombre");
+                String cargo = result.getString("cargo");
+                String telefono = result.getString("telefono");
+                String domicilio = result.getString("domicilio");
+                Date fecha_contrato = result.getDate("fecha_contrato");
 
-            registros = state.executeUpdate();
-            if(registros>0)
-                System.out.println("Registro actualizado");
+                empleado = new Empleado(codigo, nombre, cargo, telefono, domicilio, fecha_contrato);
+            }
 
-            Conexion.close(state);
-            Conexion.close(conn);
-        }catch (SQLException e) {
+            connection.close();
+            state.close();
+        } catch (SQLException e) {
             e.printStackTrace();
         }
-    }
-
-    public void modificarDom(int Codigo, String Domicilio) throws SQLException{
-        Connection conn = null;
-        PreparedStatement state = null;
-        int registros = 0;
-
-        try{
-            conn = Conexion.getConnection();
-            state = conn.prepareStatement(updateSQL);
-
-            state.setString(1, Domicilio);
-            state.setInt(2, Codigo);
-
-            registros = state.executeUpdate();
-            if(registros>0)
-                System.out.println("Registro actualizado");
-
-            Conexion.close(state);
-            Conexion.close(conn);
-        }catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void modificarFech(int Codigo, String Fech_Con) throws SQLException{
-        Connection conn = null;
-        PreparedStatement state = null;
-        int registros = 0;
-
-        try{
-            conn = Conexion.getConnection();
-            state = conn.prepareStatement(updateSQL);
-
-            state.setString(1, Fech_Con);
-            state.setInt(2, Codigo);
-
-            registros = state.executeUpdate();
-            if(registros>0)
-                System.out.println("Registro actualizado");
-
-            Conexion.close(state);
-            Conexion.close(conn);
-        }catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public Empleado buscar(int Codigo) throws SQLException{
-
-        Connection conn = null;
-        PreparedStatement state = null;
-        ResultSet result = null;
-
-        conn = Conexion.getConnection();
-        state = conn.prepareStatement(consultSQL);
-
-        Empleado empleado = new Empleado(result.getInt("Codigo"),
-                result.getString("Nombre"),
-                result.getString("Cargo"),
-                result.getString("Telefono"),
-                result.getString("Domicilio"),
-                result.getString("Fech_Con"));
-        Conexion.close(result);
-        Conexion.close(state);
-
         return empleado;
     }
 
-    public void borrar(int Codigo) throws SQLException{
-        Connection conn = null;
-        PreparedStatement state = null;
-        int registros = 0;
+    //Borra un empleado de la tabla
+    public void borrar(int codigo) {
 
-        try{
-            conn = Conexion.getConnection();
-            state = conn.prepareStatement(deleteSQL);
+        try {
+            connection = Conexion.getConnection();
+            state = connection.prepareStatement(deleteSQL);
+            state.setInt(1, codigo);
+            state.executeUpdate();
 
-            state.setInt(1, Codigo);
-
-            registros = state.executeUpdate();
-
-            if(registros>0)
-                System.out.println("Registro eliminado");
-        }catch(Exception e){
+            connection.close();
+            state.close();
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
